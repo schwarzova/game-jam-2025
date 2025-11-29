939fc5de502806cf54f9738eb6e863dae2ff4165
Jakub Schwarz <schwarzabuk@gmail.com>
2025-11-29 13:30:58 +0100

    Player idle and walk animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -17,8 +17,10 @@ var positions = [
 	Vector3(0.3, -0.8,  -0.2), # hráč 3
 ]
 
+
 @onready var model = $Model
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var anim_player: AnimationPlayer = $playerglb/AnimationPlayer
+
 
 func _ready():
 	# základ – obarví model (pokud má MeshInstance3D s material override nebo modulate)
@@ -30,6 +32,8 @@ func move_steps(steps: int) -> void:
 		return
 
 	is_moving = true
+	anim_player.play("Walk", -1,1.0 ,true)
+	anim_player.speed_scale= 7
 	var target = min(current_tile_index + steps, board.tiles.size() - 1)
 
 	for i in range(current_tile_index + 1, target + 1):
@@ -37,6 +41,8 @@ func move_steps(steps: int) -> void:
 
 	current_tile_index = target
 	is_moving = false
+	anim_player.speed_scale= 1
+	anim_player.play("Idle")
 
 	board.on_player_finished_move(self)
 
@@ -45,11 +51,10 @@ func move_to_index(tile_index: int) -> void:
 	var tile = board.tiles[tile_index]
 	var offset: Vector3 = positions[player_index]
 	var target_pos: Vector3 = tile.player_spot.global_position + offset
-
-	# později sem dáš animace – např. anim_player.play("run")
+	
 	var tween = create_tween()
 	tween.tween_property(self, "global_position", target_pos, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
 	await tween.finished
-	# pak třeba anim_player.play("idle")
-
 	current_tile_index = tile_index
+	
